a1e38f748f4d7e86479d06beb5651b9e8d620fcb
Vladislav Doster <mvdoster@gmail.com>
2022-03-29 10:10:00 -0500

    feat: refactor out init.lua into individual modules so I DRM' block
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ ext/keybind.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.hyper = { "cmd", "alt", "ctrl" }
+
+M.bind = function(hyperfns)
+	for key, fn in pairs(hyperfns) do
+		hs.hotkey.bind(M.hyper, key, fn)
+	end
+end
+
+return M
--- init.lua
+++ init.lua
@@ -1,56 +1,27 @@
+local M = {}
+
+local K = require("ext.keybind")
+
+require("ext.spoons")
+require("ext.battery")
+require("ext.dockTime").start()
+require("ext.infoDisplay").start()
+require("ext.navigation")
+
 hs.window.animationDuration = 0.1
 hs.ipc.cliInstall()
-hyper = { 'cmd', 'alt', 'ctrl' }
-hyperfns = {}
-frameCache = {}
 
-require('ext.battery')
-require('ext.dockTime')
-require('ext.infoDisplay')
-require('ext.navigation')
-require('spoons')
+M.windows = require("ext.windows")
+M.windows.applicationToggle({
+	e = "kitty",
+	z = "Music",
+	q = "Vivaldi",
+})
 
-function toggle_application(_app)
-  local app = hs.appfinder.appFromName(_app)
-  if not app then
-    -- FIXME: This should really launch _app
-    return
-  end
-  local mainwin = app:mainWindow()
-  if mainwin then
-    if mainwin == hs.window.focusedWindow() then
-      mainwin:application():hide()
-    else
-      mainwin:application():activate(true)
-      mainwin:application():unhide()
-      mainwin:focus()
-    end
-  end
-end
+local keybinds = {}
+keybinds["h"] = hs.hints.windowHints
+-- keybinds["t"] = require("ext.sysStats").toggle
+keybinds["y"] = hs.toggleConsole
+K.bind(keybinds)
 
--- Toggle a window between its normal size, and being maximized
-function toggle_window_maximized()
-  local win = hs.window.focusedWindow()
-  if frameCache[win:id()] then
-    win:setFrame(frameCache[win:id()])
-    frameCache[win:id()] = nil
-  else
-    frameCache[win:id()] = win:frame()
-    win:maximize()
-  end
-end
--- Application hotkeys
-hyperfns['e'] = function()
-  toggle_application('kitty')
-end
-hyperfns['f'] = toggle_window_maximized
-hyperfns['h'] = hs.hints.windowHints
-hyperfns['t'] = require('ext.sysStats').toggle
-hyperfns['q'] = function()
-  toggle_application('Vivaldi')
-end
-hyperfns['y'] = hs.toggleConsole
-for _hotkey, _fn in pairs(hyperfns) do
-  hs.hotkey.bind(hyper, _hotkey, _fn)
-end
-hs.notify.new({ title = 'Hammerspoon', informativeText = 'Config loaded' }):send()
+hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
