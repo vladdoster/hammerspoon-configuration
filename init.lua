cf7b4283d350271a9777e112fba4213efeb001b4
Vladislav Doster <mvdoster@gmail.com>
2022-05-06 01:22:39 -0500

    maint: remove broken configuration modules until I can fix
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- init.lua
+++ init.lua
@@ -1,25 +1,23 @@
 local M = {}
 local K = require 'ext.keybind'
+
 hs.window.animationDuration = 0.1
--- Ensure the IPC command line client is available
 hs.ipc.cliInstall()
-require 'ext.spoons'
+
 require 'ext.battery'
-require 'ext.navigation'
+require 'ext.spoons'
 require 'ext.volume'
-require 'ext.border'
 require('ext.dockTime').start()
 require('ext.infoDisplay').start()
-require('ext.isOnline'):start()
-M.windows = require 'ext.windows'
-M.windows.applicationToggle {e='kitty', z='Music', q='Vivaldi'}
-local keybinds = {}
-keybinds['h'] = hs.hints.windowHints
-keybinds['t'] = require('ext.sysStats').toggle
-keybinds['y'] = hs.toggleConsole
-K.bind(keybinds)
+
 local function reload()
   hs.reload()
   hs.alert.show('hammerspoon re-loaded')
 end
-hs.hotkey.bind({'ctrl', 'cmd'}, 'R', reload)
+
+local keybinds = {}
+keybinds['h'] = hs.hints.windowHints
+keybinds['t'] = require('ext.sysStats').toggle
+keybinds['y'] = hs.toggleConsole
+keybinds['R'] = reload
+K.bind(keybinds)
