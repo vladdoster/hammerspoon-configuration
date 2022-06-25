df11d2a52d1db6925697f86a0a462016e36ac6c1
Vladislav Doster <mvdoster@gmail.com>
2022-06-25 06:05:03 -0500

    feat: add less-laggy clipboard HS module
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- init.lua
+++ init.lua
@@ -1,23 +1,22 @@
-local M = {}
-local K = require 'ext.keybind'
-
 hs.window.animationDuration = 0.1
 hs.ipc.cliInstall()
 
+local K = require('ext.keybind')
 require 'ext.battery'
 require 'ext.spoons'
 require 'ext.volume'
-require('ext.dockTime').start()
-require('ext.infoDisplay').start()
+require 'ext.clipboard'
+require 'ext.dockTime'.start()
+require 'ext.infoDisplay'.start()
 
 local function reload()
   hs.reload()
   hs.alert.show('hammerspoon re-loaded')
 end
 
-local keybinds = {}
-keybinds['h'] = hs.hints.windowHints
-keybinds['t'] = require('ext.sysStats').toggle
-keybinds['y'] = hs.toggleConsole
-keybinds['R'] = reload
-K.bind(keybinds)
+local keymaps = {}
+keymaps['h'] = hs.hints.windowHints
+keymaps['t'] = require('ext.sysStats').toggle
+keymaps['y'] = hs.toggleConsole
+keymaps['R'] = reload
+K.bind(keymaps)
