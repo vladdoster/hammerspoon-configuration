e925c8f406fd6672c0d1fbcee44926f06e9dc3aa
Vladislav Doster <mvdoster@gmail.com>
2023-04-08 15:32:54 -0500

    fix: check if space did not change
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- ext/app-toggle.lua
+++ ext/app-toggle.lua
@@ -1,31 +1,44 @@
+-- vim: set expandtab filetype=lua shiftwidth=2 softtabstop=2 tabstop=2:
+
+local obj={}
+obj.__index = obj
+-- Metadata
+obj.name = "AppToggle"
+obj.version = "0.5"
+obj.author = "Vladislav Doster <mvdoster@gmail.com>"
+obj.homepage = "https://github.com/Hammerspoon/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+local spaces, fnutils = require('hs.spaces'), require('hs.fnutils')
+local getSetting = function(label, default) return hs.settings.get(obj.name.."."..label) or default end
+local setSetting = function(label, value)   hs.settings.set(obj.name.."."..label, value); return value end
+obj.frequency = 0.8
+obj.hist_size = 100
+obj.honor_ignoredidentifiers = true
+obj.paste_on_select = getSetting('paste_on_select', false)
+obj.logger = hs.logger.new('AppToggler')
+obj.logger.setLogLevel('info')
+
 ext = {app={}, cache={}}
 hs.spaces.MCwaitTime = 0.1
 
 function ext.app.forceLaunchOrFocus(appName)
-  -- first focus with hammerspoon
   hs.application.launchOrFocus(appName)
-
-  -- clear timer if exists
   if ext.cache.launchTimer then ext.cache.launchTimer:stop() end
-
-  -- wait 500ms for window to appear and try hard to show the window
   ext.cache.launchTimer = hs.timer.doAfter(0.5, function()
     local frontmostApp = hs.application.frontmostApplication()
-    local frontmostWindows = hs.fnutils.filter(frontmostApp:allWindows(), function(win) return win:isStandard() end)
-
+    local frontmostWindows = fnutils.filter(frontmostApp:allWindows(), function(win) return win:isStandard() end)
     -- break if this app is not frontmost (when/why?)
     if frontmostApp:title() ~= appName then
-      print('Expected app in front: ' .. appName .. ' got: ' .. frontmostApp:title())
+      obj.logger.i("front most app " .. frontmostApp:title())
       return
     end
-
     if #frontmostWindows == 0 then
       -- check if there's app name in window menu (Calendar, Messages, etc...)
       if frontmostApp:findMenuItem({'Window', appName}) then
         -- select it, usually moves to space with this window
         frontmostApp:selectMenuItem({'Window', appName})
       else
-        -- otherwise send cmd-n to create new window
+        obj.logger.i("creating new window " .. appName)
         hs.eventtap.keyStroke({'cmd'}, 'n')
       end
     end
@@ -37,16 +50,12 @@ function ext.app.smartLaunchOrFocus(launchApps)
   local frontmostWindow = hs.window.frontmostWindow()
   local runningApps = hs.application.runningApplications()
   local runningWindows = {}
-
   -- filter running applications by apps array
-  local runningApps = hs.fnutils.map(launchApps, function(launchApp) return hs.application.get(launchApp) end)
-
+  local runningApps = fnutils.map(launchApps, function(launchApp) return hs.application.get(launchApp) end)
   -- create table of sorted windows per application
-  hs.fnutils.each(runningApps, function(runningApp)
-    local standardWindows = hs.fnutils.filter(runningApp:allWindows(), function(win) return win:isStandard() end)
-
+  fnutils.each(runningApps, function(runningApp)
+    local standardWindows = fnutils.filter(runningApp:allWindows(), function(win) return win:isStandard() end)
     table.sort(standardWindows, function(a, b) return a:id() < b:id() end)
-
     runningWindows = standardWindows
   end)
 
@@ -58,8 +67,7 @@ function ext.app.smartLaunchOrFocus(launchApps)
     ext.app.forceLaunchOrFocus(runningApps[1]:title())
   else
     -- check if one of windows is already focused
-    local currentIndex = hs.fnutils.indexOf(runningWindows, frontmostWindow)
-
+    local currentIndex = fnutils.indexOf(runningWindows, frontmostWindow)
     if not currentIndex then
       -- if none of them is selected focus the first one
       runningWindows[1]:focus()
@@ -67,23 +75,26 @@ function ext.app.smartLaunchOrFocus(launchApps)
       -- otherwise cycle through all the windows
       local newIndex = currentIndex + 1
       if newIndex > #runningWindows then newIndex = 1 end
-      if runningWindows[newIndex]:isFullScreen() then
-        hs.spaces.gotoSpace(hs.spaces.windowSpaces(runningWindows[newIndex])[newIndex])
-        hs.spaces.closeMissionControl()
+      local currentSpace = spaces.focusedSpace()
+      local windowSpace = spaces.windowSpaces(runningWindows[newIndex])[newIndex]
+      obj.logger.i("current space: " .. currentSpace .. " window space: " .. windowSpace)
+      if runningWindows[newIndex]:isFullScreen() and windowSpace ~= currentSpace then
+        obj.logger.i("focusing space: " .. windowSpace)
+        spaces.gotoSpace(spaces.windowSpaces(runningWindows[newIndex])[newIndex])
+        spaces.closeMissionControl()
       end
       runningWindows[newIndex]:focus()
     end
   end
 end
-
 -- keyboard modifiers for bindings
 local mod = {cc={'cmd', 'ctrl'}, ca={'cmd', 'alt'}, cac={'cmd', 'alt', 'ctrl'}, cas={'cmd', 'alt', 'shift'}}
-
 -- launch and focus applications
-hs.fnutils.each({
+fnutils.each({
   {key='b', apps={'Safari', 'Google Chrome', 'Vivaldi'}},
   {key='f', apps={'Finder'}},
   {key='m', apps={'Teams'}},
   {key='s', apps={'System Settings'}},
+  {key='y', apps={'Hammerspoon'}},
   {key='t', apps={'WezTerm'}}
 }, function(object) hs.hotkey.bind(mod.cac, object.key, function() ext.app.smartLaunchOrFocus(object.apps) end) end)
