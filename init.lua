7efbccee481efeebbb39281e80c51b2c3ea4a14e
Vladislav Doster <mvdoster@gmail.com>
2026-08-17 16:35:22 -0500

    fix(config): ignore loginwindow in window filter root
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ require("hs.ipc")
 
 hs.window.animationDuration = 0.1
 
+-- loginwindow ships only in ignoreInDefaultFilter, which gates windows and not app registration, so wfilter warns about its id-0 phantom window until it is ignored at the root
+hs.window.filter.ignoreAlways["loginwindow"] = true
+
 hs.loadSpoon("SpoonInstall")
 
 spoon.SpoonInstall:andUse("BatteryMonitor", { start = true })
